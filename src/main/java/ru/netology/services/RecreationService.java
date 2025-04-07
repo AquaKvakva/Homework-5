f7223315f64cf6cbd46ca9eab027e59aa8b2fcaa
Olga Sukhareva <3830921@gmail.com>
2025-04-07 12:17:02 +0300

    division change

--- src/main/java/ru/netology/services/RecreationService.java
+++ src/main/java/ru/netology/services/RecreationService.java
@@ -8,7 +8,7 @@ public class RecreationService {
         for (int month = 1; month <= 12; month++) {
             if (balance >= threshold) {
                 count++;
-                balance = (balance - expense) * 34 / 100 ;
+                balance = (balance - expense) / 3 ;
             } else {
                 balance = balance + income - expense;
             }
